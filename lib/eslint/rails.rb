c3bd9eef90b5a08183bd8869bf1505901eb8417b
Justin Force <justin.force@gmail.com>
2015-03-12 15:01:42 -0700

    Initial commit
    `bundle gem xxx` boilerplate.

---
+++ lib/eslint/rails.rb
@@ -0,0 +1,7 @@
+require "eslint/rails/version"
+
+module Eslint
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/eslint/rails/version.rb
@@ -0,0 +1,5 @@
+module Eslint
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
