5b2b80150ff3c2dfce9e48d4714f7c54371bf822
Derek Kniffin <derek.kniffin@gmail.com>
2016-08-23 13:22:20 -0400

    Some refactoring

--- lib/eslint-rails/runner.rb
+++ lib/eslint-rails/runner.rb
@@ -9,6 +9,18 @@ module ESLintRails
     end
 
     def run
+      warnings = assets.map do |asset|
+        generate_warnings(asset).tap { |warnings| output_progress(warnings) }
+      end
+      
+      puts
+      
+      warnings.flatten
+    end
+    
+    private
+    
+    def assets
       all_assets = Rails.application.assets
 
       assets = case @filename
@@ -17,37 +29,45 @@ module ESLintRails
                else
                  [all_assets[@filename]]
                end
-
       assets.reject!{|a| a.to_s =~ /eslint.js|vendor|gems|min.js|editorial/ }
-
-      eslint_js = Rails.application.assets['eslint'].to_s
-
-      assets.map do |asset|
-        relative_path = asset.relative_path_from(Pathname.new(Dir.pwd))
-        file_content = asset.read
-
-        warning_hashes = ExecJS.eval <<-JS
-          function () {
-            window = this;
-            #{eslint_js};
-            return eslint.verify('#{escape_javascript file_content}', #{Config.read});
-          }()
-        JS
-        warnings = warning_hashes.map{|hash| ESLintRails::Warning.new(relative_path, hash)}
-
-        file_severity = warnings.map(&:severity).uniq.sort.first
-
-        print case file_severity
-              when :high
-                'H'.red
-              when :low
-                'L'.yellow
-              else
-                '.'.green
-              end
-
-        warnings
-      end.flatten
+    end
+    
+    def eslint_js
+      @eslint_js ||= Rails.application.assets['eslint'].to_s
+    end
+    
+    def warning_hashes(file_content)
+      ExecJS.eval <<-JS
+        function () {
+          window = this;
+          #{eslint_js};
+          return eslint.verify('#{escape_javascript(file_content)}', #{Config.read});
+        }()
+      JS
+    end
+    
+    def generate_warnings(asset)
+      relative_path = asset.relative_path_from(Pathname.new(Dir.pwd))
+      file_content = asset.read
+      
+      warning_hashes(file_content).map do |hash|
+        ESLintRails::Warning.new(relative_path, hash)
+      end
+    end
+    
+    def output_progress(warnings)
+      print case file_severity(warnings)
+            when :high
+              'H'.red
+            when :low
+              'L'.yellow
+            else
+              '.'.green
+            end
+    end
+    
+    def file_severity(warnings)
+      warnings.map(&:severity).uniq.sort.first
     end
   end
 end
